2d22ad2362620434ac06eddaa0aad68e6ccedbf3
Marcel Stör <marcelstoer@users.noreply.github.com>
2017-04-23 13:21:17 +0200

    First step for Travis build

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Run a build for all images.
+
+set -uo pipefail
+
+info() {
+  printf "%s\n" "$@"
+}
+
+fatal() {
+  printf "**********\n"
+  printf "%s\n" "$@"
+  printf "**********\n"
+  exit 1
+}
+
+info "Building ..."
+docker build -t netceteragroup/rpi-ttn-gateway
+
+if [[ $? -gt 0 ]]; then
+  fatal "Build failed!"
+else
+  info "Build succeeded."
+fi
+
+exit 0
